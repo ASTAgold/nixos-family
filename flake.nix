6f6a98c7b516101ecf942b0c8303d65268553253
ASTAgold <abde2006llah@gmail.com>
2025-08-21 21:37:28 +0100

    feat: Initial configuration for stewie

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-25.05 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.stewie = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./hosts/stewie/configuration.nix
+        ./modules/user.nix
+      ];
+    };
+  };
+}
---
+++ modules/user.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  users.users.admin = {
+    isNormalUser = true;
+    shell = pkgs.fish;
+    description = "with great power comes great responsibility";
+    extraGroups = [ "networkmanager" "docker" "wheel" ];
+    packages = with pkgs; [];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIER4S+aFma+Vhg7zR1smua2plzOy8R/TPArb14+zO3oL abde2006llah@gmail.com"
+    ];
+  };
+}
