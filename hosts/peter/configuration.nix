e71bf3c0c0740bfd17a25d3e7ad0d2fbfba27d6c
ASTAgold <abde2006llah@gmail.com>
2025-08-26 17:23:17 +0100

    more docker stuff

--- hosts/peter/configuration.nix
+++ hosts/peter/configuration.nix
@@ -12,6 +12,7 @@
       ./../../modules/samba.nix
       # ./../../modules/podman.nix
       ./../../modules/docker.nix
+      ./../../modules/komodo.nix
     ];
 
   
@@ -73,10 +74,6 @@
     snapraid
   ];
 
-networking.firewall.enable = true;
-networking.firewall.allowPing = true;
-
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -94,7 +91,7 @@ networking.firewall.allowPing = true;
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
